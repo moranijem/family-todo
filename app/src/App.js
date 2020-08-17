1572809959d02535ca9220009b4fff5bc0b97915
Moran <moranijem@gmail.com>
2020-08-17 14:42:04 +0300

    test1

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/Register">Register</Link>
+            </li>
+            <li>
+              <Link to="/ToDo">ToDo</Link>
+            </li>
+            <li>
+              <Link to="/Mission">Mission</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Register">
+            <Register />
+          </Route>
+          <Route path="/ToDo">
+            <ToDo />
+          </Route>
+          <Route path="/Mission">
+            <Mission/>
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+function Login() {
+  return(
+    <div className="homepage">
+    <h2>Login</h2>
+    <label for="">User-Name</label>
+    <label for="">User-LName</label>
+    <label for="">Useremil</label>
+    <label for="">Img-Url</label>
+    <label for="">Password</label>
+    </div>
+    
+
+  ) 
+}
+
+function Register() {
+  return <h2>Register</h2>;
+}
+
+function ToDo() {
+  return <h2>ToDo</h2>;
+}
+
+function Mission() {
+  return <h2>Mission</h2>;
+}
+
 
 export default App;
