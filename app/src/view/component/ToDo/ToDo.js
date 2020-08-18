e3cc91afa83ce0677a6b60171141f4a3bbf9c770
Marshood Ayoub <marshood.ayoub@gmail.com>
2020-08-18 14:06:12 +0300

    change 0.3.1

--- app/src/view/component/ToDo/ToDo.js
+++ app/src/view/component/ToDo/ToDo.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ToDo.css'
+import TodoBox from './TodoBox'
 function ToDo() {
-    return (
-        <div id='root'>
-            <h2>ToDo</h2>
+    const [dataTodo, setDataTodo] = useState([]);
+    useEffect(() => {
+         fetch('/get/toDo', {
+            method: 'POST',
+            body: JSON.stringify({ lastName: localStorage.getItem("lastName") }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                 setDataTodo(data);
 
+            })
+    }, []);
+ 
+    return (
+        <div id='root123'>
+            <h2>ToDo</h2>
+            <div className="boxtest">
+            {
+                dataTodo.map((todoTasks, index) => {
+                    return (<TodoBox key={index} todoTasks={todoTasks} />)
+                })
+            }
+            </div>
         </div>
 
     )
